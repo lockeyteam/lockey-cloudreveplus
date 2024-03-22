9b32ffc45f5e30350a320f1c47111a0a5d04cae4
lockeyteam <123921110+lockeyteam@users.noreply.github.com>
2024-03-22 20:30:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@
 #download cloudreveplus
     curl -L https://github.com/lockeyteam/lockey-cloudreveplus/releases/download/latest/cloudreveplus-linux-amd64v2.zip -o /home/cloudreveplus-linux-amd64v2.zip
     cd /home
-    unzip cloudreveplus-linux-amd64v2.zip
-    rm cloudreveplus-linux-amd64v2.zip
-	chmod +x /home/cloudreveplus
+    unzip /home/cloudreveplus-linux-amd64v2.zip
+    rm /home/cloudreveplus-linux-amd64v2.zip
+    mv cloudreveplus-linux-amd64v2 cloudreveplus
+	chmod 777 -R /home
 
 #run
 
